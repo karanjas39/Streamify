a996b930a23d792db537f82ff6db58de0cee8200
karanjas39 <dhillonjaskaran4486@gmail.com>
2024-10-02 22:23:59 +0530

    setup init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div className="text-center text-red-950 text-3xl">
+      <Button variant="destructive">Click me</Button>
+    </div>
+  );
+}
+
+export default App;
